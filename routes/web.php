6ae5e515ef7bc8507b03b54fceb3cea9eec34290
peimeng <931692760@qq.com>
2019-05-14 16:36:50 +0800

    前台 收藏列表

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ Auth::routes(['verify' => true]);
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+Route::get('products/{product}', 'ProductsController@show')
+    ->name('products.show')
+    ->where(['product'=>'[0-9]+']);
 
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function () {
@@ -13,5 +15,5 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
 
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
-
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 });
