ff128c7290f37b77a1bb081c576ced7edca0b5c4
peimeng <931692760@qq.com>
2019-05-14 14:59:43 +0800

    前台 商品详情

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ Auth::routes(['verify' => true]);
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function () {
