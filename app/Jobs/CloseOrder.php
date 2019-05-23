5820dab7060f6d0632cd4d3026a3bf72e9022cce
peimeng <931692760@qq.com>
2019-05-23 16:15:50 +0800

    订单失效

---
+++ app/Jobs/CloseOrder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class CloseOrder implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $order;
+
+    /**
+     * CloseOrder constructor.
+     * @param Order $order
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // 判断对应的订单是否已经被支付
+        // 如果已经支付则不需要关闭订单，直接退出
+        if ($this->order->paid_at) {
+            return;
+        }
+
+        \DB::transaction(function () {
+            $this->order->update(['closed'=>true]);
+            foreach($this->order->items as $item){
+                $item->productSku->addStock($item->amount);
+            }
+        });
+    }
+}
--- config/app.php
+++ config/app.php
@@ -15,6 +15,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'order_ttl' => 1800,//订单失效时间 秒
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
