0fba08632288e531f1fd34e5f98a5724fa081247
peimeng <931692760@qq.com>
2019-05-30 11:13:22 +0800

    确认收货

--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,14 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('orders','OrdersController@index')->name('orders.index');
     Route::post('orders','OrdersController@store')->name('orders.store');
     Route::get('orders/{order}','OrdersController@show')->name('orders.show');
+    //确认收货
+    Route::post('orders/{order}/received', 'OrdersController@received')->name('orders.received');
 
     //订单支付
     Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
     //支付宝前端回调
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
+
 });
 //支付宝服务端回调
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
